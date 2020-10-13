042a525436b6491dfaf6952f61aebcadb6e38bd5
Ryszard Matula <56454967+rmatu@users.noreply.github.com>
2020-10-13 19:09:44 +0200

    Update script.js

--- simpsonWeb/src/script.js
+++ simpsonWeb/src/script.js
@@ -3,54 +3,20 @@ const glasses = document.getElementById('glasses');
 const shirt = document.getElementById('shirt');
 const trousers = document.getElementById('trousers');
 
-hair.onclick = () => {
-  const avatarHair = document.getElementById('avatar-hair');
-  const span = hair.getElementsByTagName('span')[0];
+hair.onclick = () => changeItem(hair);
+glasses.onclick = () => changeItem(glasses);
+shirt.onclick = () => changeItem(shirt);
+trousers.onclick = () => changeItem(trousers);
 
-  if (avatarHair.style.display === 'none') {
-    avatarHair.style.display = 'block';
-    span.innerHTML = 'usuń';
+const changeItem = (item) => {
+  const element = document.getElementById('avatar-' + item.id);
+  const span = item.getElementsByTagName('span')[0];
+
+  if (element.style.display === 'none') {
+    element.style.display = 'block';
+    span.innerHtlm = 'usuń';
   } else {
-    avatarHair.style.display = 'none';
-    span.innerHTML = 'dodaj';
-  }
-};
-
-glasses.onclick = () => {
-  const avatarGlasses = document.getElementById('avatar-glasses');
-  const span = glasses.getElementsByTagName('span')[0];
-
-  if (avatarGlasses.style.display === 'none') {
-    avatarGlasses.style.display = 'block';
-    span.innerHTML = 'usuń';
-  } else {
-    avatarGlasses.style.display = 'none';
-    span.innerHTML = 'dodaj';
-  }
-};
-
-shirt.onclick = () => {
-  const avatarShirt = document.getElementById('avatar-shirt');
-  const span = shirt.getElementsByTagName('span')[0];
-
-  if (avatarShirt.style.display === 'none') {
-    avatarShirt.style.display = 'block';
-    span.innerHTML = 'usuń';
-  } else {
-    avatarShirt.style.display = 'none';
-    span.innerHTML = 'dodaj';
-  }
-};
-
-trousers.onclick = () => {
-  const avatarTrousers = document.getElementById('avatar-trousers');
-  const span = trousers.getElementsByTagName('span')[0];
-
-  if (avatarTrousers.style.display === 'none') {
-    avatarTrousers.style.display = 'block';
-    span.innerHTML = 'usuń';
-  } else {
-    avatarTrousers.style.display = 'none';
-    span.innerHTML = 'dodaj';
+    element.style.display = 'none';
+    span.innerHtml = 'dodaj';
   }
 };
