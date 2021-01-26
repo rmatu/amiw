1a183f8c822a71bdb9299c9db0c62392adc4ea2d
Ryszard Matula <rmatula@us.edu.pl>
2021-01-26 10:17:44 +0100

    UI update

--- bootstrap-app/src/app/app.component.ts
+++ bootstrap-app/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+interface Alert {
+  type: string;
+  message: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bootstrap-app';
